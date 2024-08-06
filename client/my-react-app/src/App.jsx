33edc7f9738729ad96b944f624e1e111271af62e
arash-malekzadeh <arash.malekzade@gmail.com>
2024-08-06 22:05:38 +0330

    routes and pages first deine

---
+++ client/my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
